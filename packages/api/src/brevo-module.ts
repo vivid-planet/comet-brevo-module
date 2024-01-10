ce2f5e29d14962c3054710cb43a8370ad4140dee
Denise Buder <denise.buder@vivid-planet.com>
2024-01-10 15:57:41 +0100

    remove duplicate brevo module

--- packages/api/src/brevo-module.ts
+++ packages/api/src/brevo-module.ts
@@ -1,11 +1,16 @@
 import { DynamicModule, Global, Module } from "@nestjs/common";
 
+import { BrevoContactModule } from "./brevo-contact/brevo-contact.module";
+import { CometBrevoModuleConfig } from "./config/comet-brevo-module.config";
+import { ConfigModule } from "./config/config.module";
+
 @Global()
 @Module({})
 export class BrevoModule {
-    static register(): DynamicModule {
+    static register(config: CometBrevoModuleConfig): DynamicModule {
         return {
             module: BrevoModule,
+            imports: [BrevoModule, BrevoContactModule, ConfigModule.forRoot(config)],
             exports: [],
         };
     }
