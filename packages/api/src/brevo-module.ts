9dde317acb31de86d275e1c75f2db52fa3ae9840
Denise Buder <denise.buder@vivid-planet.com>
2024-01-10 16:35:16 +0100

    remove comet prefix

--- packages/api/src/brevo-module.ts
+++ packages/api/src/brevo-module.ts
@@ -1,13 +1,13 @@
 import { DynamicModule, Global, Module } from "@nestjs/common";
 
 import { BrevoContactModule } from "./brevo-contact/brevo-contact.module";
-import { CometBrevoModuleConfig } from "./config/comet-brevo-module.config";
+import { BrevoModuleConfig } from "./config/brevo-module.config";
 import { ConfigModule } from "./config/config.module";
 
 @Global()
 @Module({})
 export class BrevoModule {
-    static register(config: CometBrevoModuleConfig): DynamicModule {
+    static register(config: BrevoModuleConfig): DynamicModule {
         return {
             module: BrevoModule,
             imports: [BrevoModule, BrevoContactModule, ConfigModule.forRoot(config)],
