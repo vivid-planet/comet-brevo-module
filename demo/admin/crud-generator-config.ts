74e9e41e67857a0b0864868e504ea91fd7daa0ec
Denise Buder <denise.buder@vivid-planet.com>
2024-01-22 14:13:08 +0100

    generate basic brevo contact page files in demo projec in demo project

---
+++ demo/admin/crud-generator-config.ts
@@ -0,0 +1,7 @@
+import { CrudGeneratorConfig } from "@comet/cms-admin";
+export default [
+    {
+        target: "src/brevoContacts/generated",
+        entityName: "BrevoContact",
+    },
+] satisfies CrudGeneratorConfig[];
