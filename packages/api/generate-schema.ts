08929f88bf7ec9ed8d8e01ddf608c1634c3ee746
Denise Buder <denise.buder@vivid-planet.com>
2024-01-24 15:57:47 +0100

    add missing empty scope for schema generation

--- packages/api/generate-schema.ts
+++ packages/api/generate-schema.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from "@nestjs/core";
-import { GraphQLSchemaBuilderModule, GraphQLSchemaFactory } from "@nestjs/graphql";
+import { Field, GraphQLSchemaBuilderModule, GraphQLSchemaFactory, InputType, ObjectType } from "@nestjs/graphql";
 import { writeFile } from "fs/promises";
 import { printSchema } from "graphql";
 
 import { createBrevoContactResolver } from "./src/brevo-contact/brevo-contact.resolver";
 import { BrevoContactFactory } from "./src/brevo-contact/dto/brevo-contact.factory";
 import { SubscribeInputFactory } from "./src/brevo-contact/dto/subscribe-input.factory";
+import { EmailCampaignScopeInterface } from "./src/types";
+
+@ObjectType("EmailCampaignContentScope")
+@InputType("EmailCampaignContentScopeInput")
+class EmailCampaignScope implements EmailCampaignScopeInterface {
+    [key: string]: unknown;
+    // empty scope
+    @Field({ nullable: true })
+    thisScopeHasNoFields____?: string; // just anything so this class has at least one field and can be interpreted as a gql-object/input type
+}
 
 async function generateSchema(): Promise<void> {
     console.info("Generating schema.gql...");
@@ -15,8 +25,8 @@ async function generateSchema(): Promise<void> {
     const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
 
     const BrevoContact = BrevoContactFactory.create({});
-    const BrevoContactSubscribeInput = SubscribeInputFactory.create({});
-    const BrevoContactResolver = createBrevoContactResolver({ BrevoContact, BrevoContactSubscribeInput });
+    const BrevoContactSubscribeInput = SubscribeInputFactory.create({ Scope: EmailCampaignScope });
+    const BrevoContactResolver = createBrevoContactResolver({ BrevoContact, BrevoContactSubscribeInput, Scope: EmailCampaignScope });
 
     const schema = await gqlSchemaFactory.create([BrevoContactResolver]);
 
