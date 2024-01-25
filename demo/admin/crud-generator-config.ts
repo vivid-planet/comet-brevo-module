b833af97959ab51ff41985af3bea241d0845274e
Denise Buder <denise.buder@vivid-planet.com>
2024-01-25 15:04:55 +0100

    move generated target group admin files to package

--- demo/admin/crud-generator-config.ts
+++ demo/admin/crud-generator-config.ts
@@ -1,7 +1,2 @@
 import { CrudGeneratorConfig } from "@comet/cms-admin";
-export default [
-    {
-        target: "src/targetGroup/generated",
-        entityName: "TargetGroup",
-    },
-] satisfies CrudGeneratorConfig[];
+export default [] satisfies CrudGeneratorConfig[];
