d617eed81e15eded5b1cad65e19cf255d94830bb
Denise Buder <denise.buder@vivid-planet.com>
2024-01-29 14:58:10 +0100

    move generated emailCampaign files to module

--- demo/admin/crud-generator-config.ts
+++ demo/admin/crud-generator-config.ts
@@ -1,13 +1,2 @@
 import { CrudGeneratorConfig } from "@comet/cms-admin";
-export default [
-    {
-        target: "src/emailCampaigns/generated",
-        entityName: "EmailCampaign",
-        rootBlocks: {
-            emailContentBlock: {
-                name: "EmailCampaignContentBlock",
-                import: "../emailCampaigns/blocks/EmailCampaignContentBlock",
-            },
-        },
-    },
-] satisfies CrudGeneratorConfig[];
+export default [] satisfies CrudGeneratorConfig[];
