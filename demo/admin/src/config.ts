7c477aaf45f8b878dcc7d2969092140722b61ed1
Stefanie Kaltenhauser <stefanie.kaltenhauser@vivid-planet.com>
2024-05-06 15:03:48 +0200

    Define and use type of sites config

--- demo/admin/src/config.ts
+++ demo/admin/src/config.ts
@@ -1,3 +1,5 @@
+import { SiteConfig } from "@comet/cms-admin";
+
 import cometConfig from "../comet-config.json";
 import { environment } from "./environment";
 
@@ -18,7 +20,7 @@ export function createConfig() {
         ...cometConfig,
         apiUrl: environmentVariables.API_URL,
         adminUrl: environmentVariables.ADMIN_URL,
-        sitesConfig: JSON.parse(environmentVariables.SITES_CONFIG),
+        sitesConfig: JSON.parse(environmentVariables.SITES_CONFIG) as SitesConfig,
         buildDate: environmentVariables.BUILD_DATE,
         buildNumber: environmentVariables.BUILD_NUMBER,
         commitSha: environmentVariables.COMMIT_SHA,
@@ -29,3 +31,5 @@ export function createConfig() {
 export const config = createConfig();
 
 export type Config = ReturnType<typeof createConfig>;
+
+export type SitesConfig = Record<string, SiteConfig>;
