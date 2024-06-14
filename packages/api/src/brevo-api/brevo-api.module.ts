be65ad0c30029c3c98c6e417e81a075bd3be3da5
Denise Buder <denise.buder@vivid-planet.com>
2024-06-14 13:23:45 +0200

    Add new brevo services to brevo api module

--- packages/api/src/brevo-api/brevo-api.module.ts
+++ packages/api/src/brevo-api/brevo-api.module.ts
@@ -1,13 +1,27 @@
 import { Module } from "@nestjs/common";
 
 import { ConfigModule } from "../config/config.module";
+import { BrevoApiAccountService } from "./brevo-api-account.service";
 import { BrevoApiCampaignsService } from "./brevo-api-campaigns.service";
 import { BrevoApiContactsService } from "./brevo-api-contact.service";
 import { BrevoApiSenderService } from "./brevo-api-sender.service";
+import { BrevoApiTransactionalEmailsApiService } from "./brevo-api-transactional-emails.service";
 
 @Module({
     imports: [ConfigModule],
-    providers: [BrevoApiContactsService, BrevoApiCampaignsService, BrevoApiSenderService],
-    exports: [BrevoApiContactsService, BrevoApiCampaignsService, BrevoApiSenderService],
+    providers: [
+        BrevoApiContactsService,
+        BrevoApiCampaignsService,
+        BrevoApiSenderService,
+        BrevoApiTransactionalEmailsApiService,
+        BrevoApiAccountService,
+    ],
+    exports: [
+        BrevoApiContactsService,
+        BrevoApiCampaignsService,
+        BrevoApiSenderService,
+        BrevoApiTransactionalEmailsApiService,
+        BrevoApiAccountService,
+    ],
 })
 export class BrevoApiModule {}
