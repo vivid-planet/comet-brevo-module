b1ca84df275ab085eb6c548cada68edcece1d4b2
Denise Buder <denise.buder@vivid-planet.com>
2024-08-06 12:53:27 +0200

    Add better comment for migration in demo project

--- demo/api/src/db/migrations/Migration20240802111659.ts
+++ demo/api/src/db/migrations/Migration20240802111659.ts
@@ -1,8 +1,7 @@
 import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20240802111659 extends Migration {
-    // TODO: should this be generated automatically by the migration generator? currently needs to be added manually
-    // TODO: must entity generation be done like the pagetreenode in comet? currently it is done like redirect
+    // this migration needs to be generated manually
     async up(): Promise<void> {
         this.addSql('alter table "EmailCampaign" add column "scope_domain" text not null, add column "scope_language" text not null;');
         this.addSql(
