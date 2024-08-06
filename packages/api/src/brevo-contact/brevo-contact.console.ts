d21db92dccebc86de398fda7fa1a3d92130e8b40
juliawegmayr <109900447+juliawegmayr@users.noreply.github.com>
2024-08-06 10:25:53 +0200

    COM-687 Add automatic deletion process (#59)
    * add job for deleting blacklistedContacts
    
    * add changeset
    
    * fix prettier error
    
    * refactor findNonMainTargetGroups to findMainTargetGroups including where parameter
    
    * add where parameter to brevo-contacts-services functions
    
    * use findMainTargetGroups function to get scopes for deleting contacts
    
    * fix prettier problems
    
    * fix prettier problems
    
    * add @CreateRequestContext to execute function
    
    * fix pipeline
    
    * correct scope by using dynamic values
    
    * rename variable numberOfBlacklistedContacts to hasMoreContacts
    
    * correct condition to check if more contacts are avaiable
    
    * rename findMainTargetGroups to findTargetGroups
    
    * correct type of where in findTargetGroups
    
    * use scope directly
    
    * fix lint error
    
    * adapt changeset to only include information that is relevant for the application
    
    * remove scope from findTargetGroups and pass it in the prop instead
    
    * correct hasMoreContacts
    
    * rename DeleteUnsubscribedContactsConsole to DeleteUnsubscribedBrevoContactsConsole
    
    ---------
    
    Co-authored-by: Julia Wegmayr <julia.wegmayr@vivid-planet.com>

---
+++ packages/api/src/brevo-contact/brevo-contact.console.ts
@@ -0,0 +1,49 @@
+import { CreateRequestContext, MikroORM } from "@mikro-orm/core";
+import { Injectable } from "@nestjs/common";
+import { Command, Console } from "nestjs-console";
+
+import { BrevoApiContactsService } from "../brevo-api/brevo-api-contact.service";
+import { TargetGroupsService } from "../target-group/target-groups.service";
+
+@Injectable()
+@Console()
+export class DeleteUnsubscribedBrevoContactsConsole {
+    constructor(
+        private readonly brevoApiContactsService: BrevoApiContactsService,
+        private readonly targetGroupsService: TargetGroupsService,
+        private readonly orm: MikroORM,
+    ) {}
+
+    @Command({
+        command: "delete-unsubscribed-brevo-contacts",
+        description: "deletes unsubscribed contacts",
+    })
+    @CreateRequestContext()
+    async execute(): Promise<void> {
+        const offset = 0;
+        const limit = 50;
+        const where = { isMainList: true };
+
+        const [targetGroups] = await this.targetGroupsService.findTargetGroups({ offset, limit, where });
+
+        for (const targetGroup of targetGroups) {
+            let hasMoreContacts = false;
+            let offset = 0;
+
+            do {
+                const contacts = await this.brevoApiContactsService.findContacts(limit, offset, {
+                    scope: targetGroup.scope,
+                });
+
+                const blacklistedContacts = contacts.filter((contact) => contact.emailBlacklisted === true);
+
+                if (blacklistedContacts.length > 0) {
+                    await this.brevoApiContactsService.deleteContacts(blacklistedContacts, { scope: targetGroup.scope });
+                }
+
+                hasMoreContacts = !(contacts.length < limit);
+                offset += limit;
+            } while (hasMoreContacts);
+        }
+    }
+}
