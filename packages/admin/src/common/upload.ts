39d73056f34fe0db704305867b9f4d3b092e8759
thomashuettmaier <thomas.huettmaier@vivid-planet.com>
2024-08-13 10:57:41 +0200

    fix applying existing user to current target group

--- packages/admin/src/common/upload.ts
+++ packages/admin/src/common/upload.ts
@@ -3,10 +3,11 @@ import { GQLEmailCampaignContentScopeInput } from "../graphql.generated";
 
 const config = createConfig();
 
-export function upload(file: File, scope: GQLEmailCampaignContentScopeInput, listIds?: number[]): Promise<Response> {
+export function upload(file: File, scope: GQLEmailCampaignContentScopeInput, listIds?: string[]): Promise<Response> {
     const formData = new FormData();
     formData.append("file", file);
     formData.append("scope", JSON.stringify(scope));
+
     if (listIds) formData.append("listIds", JSON.stringify(listIds));
 
     return fetch(`${config.apiUrl}/brevo-contacts-csv/upload`, {
--- packages/api/src/brevo-contact/brevo-contact-import.controller.ts
+++ packages/api/src/brevo-contact/brevo-contact-import.controller.ts
@@ -1,6 +1,9 @@
 import { CometValidationException, RequiredPermission } from "@comet/cms-api";
+import { EntityRepository } from "@mikro-orm/core";
+import { InjectRepository } from "@mikro-orm/nestjs";
 import { Body, Controller, Inject, Post, Type, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { TargetGroupInterface } from "src/target-group/entity/target-group-entity.factory";
 
 import { BrevoContactImportService } from "../brevo-contact/brevo-contact-import.service";
 import { ImporterErrorInterceptor } from "../brevo-contact/contact-importer-error.interceptor";
@@ -14,6 +17,7 @@ export function createBrevoContactImportController({ Scope }: { Scope: Type<Emai
         constructor(
             @Inject(BREVO_MODULE_CONFIG) private readonly config: BrevoModuleConfig,
             @Inject(BrevoContactImportService) private readonly brevoContactImportService: BrevoContactImportService,
+            @InjectRepository("TargetGroup") private readonly targetGroupRepository: EntityRepository<TargetGroupInterface>,
         ) {}
 
         @Post("upload")
@@ -36,10 +40,12 @@ export function createBrevoContactImportController({ Scope }: { Scope: Type<Emai
             const parsedScope = JSON.parse(scope) as EmailCampaignScopeInterface;
 
             let parsedListIds = undefined;
-            if (listIds) parsedListIds = JSON.parse(listIds) as number[];
+            if (listIds) parsedListIds = JSON.parse(listIds) as string[];
+
+            const targetGroupBrevoIds = (await this.targetGroupRepository.find({ id: { $in: parsedListIds } })).map(({ brevoId }) => brevoId);
 
             const redirectUrl = this.config.brevo.resolveConfig(parsedScope).redirectUrlForImport;
-            await this.brevoContactImportService.importContactFromCsv(contentContent, parsedScope, redirectUrl, parsedListIds);
+            await this.brevoContactImportService.importContactFromCsv(contentContent, parsedScope, redirectUrl, targetGroupBrevoIds);
         }
     }
 
