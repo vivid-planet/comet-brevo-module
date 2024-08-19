a3f02c0783a9832bf270a381896703e368628a4a
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-08-19 09:54:24 +0200

    use MjmlSection in ImageBlock instead of IndentedSectionGroup

--- demo/campaign/src/blocks/ImageBlock.tsx
+++ demo/campaign/src/blocks/ImageBlock.tsx
@@ -1,16 +1,15 @@
 import { PropsWithData } from "@comet/cms-site";
-import { MjmlColumn } from "@luma-team/mjml-react";
+import { MjmlColumn, MjmlSection } from "@luma-team/mjml-react";
 import { PixelImageBlockData } from "@src/blocks.generated";
 import { CommonImageBlock } from "@src/common/blocks/CommonImageBlock";
-import { IndentedSectionGroup } from "@src/components/IndentedSectionGroup";
 import { theme } from "@src/util/theme";
 
 export const ImageBlock = ({ data }: PropsWithData<PixelImageBlockData>) => {
     return (
-        <IndentedSectionGroup paddingLeft={0} paddingRight={0}>
+        <MjmlSection>
             <MjmlColumn>
                 <CommonImageBlock data={data} desktopRenderWidth={theme.mailSize.contentWidth} />
             </MjmlColumn>
-        </IndentedSectionGroup>
+        </MjmlSection>
     );
 };
