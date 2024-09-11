e15e4c9861f698369269cb4be351f4bcc85a4a52
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-09-11 09:47:30 +0200

    correct migration to not nullable and default false

--- packages/api/src/mikro-orm/migrations/Migration20240830112400.ts
+++ packages/api/src/mikro-orm/migrations/Migration20240830112400.ts
@@ -2,6 +2,6 @@ import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20240830112400 extends Migration {
     async up(): Promise<void> {
-        this.addSql('alter table "TargetGroup" add column "isTestList" boolean null;');
+        this.addSql('alter table "TargetGroup" add column "isTestList" boolean not null default false;');
     }
 }
