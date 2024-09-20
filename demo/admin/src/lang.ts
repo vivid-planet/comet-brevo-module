cd109b21266dd9aa58b49bc1a74ff75141eda0ed
Raphael Blum <raphael.blum@vivid-planet.com>
2024-09-20 13:38:57 +0200

    Fix i18n and lang repo handling

--- demo/admin/src/lang.ts
+++ demo/admin/src/lang.ts
@@ -2,12 +2,19 @@ import { ResolvedIntlConfig } from "react-intl";
 
 import project_messages_de from "../lang-compiled/comet-brevo-module-demo-lang/de.json";
 import project_messages_en from "../lang-compiled/comet-brevo-module-demo-lang/en.json";
-import comet_messages_de from "../lang-compiled/comet-brevo-module-lang/de.json";
-import comet_messages_en from "../lang-compiled/comet-brevo-module-lang/en.json";
+import comet_brevo_module_messages_de from "../lang-compiled/comet-brevo-module-lang/de.json";
+import comet_brevo_module_messages_en from "../lang-compiled/comet-brevo-module-lang/en.json";
+import comet_messages_de from "../lang-compiled/comet-lang/de.json";
+import comet_messages_en from "../lang-compiled/comet-lang/en.json";
 
 const cometMessages = {
     en: comet_messages_en,
     de: comet_messages_de,
+};
+
+const cometBrevoModuleMessages = {
+    en: comet_brevo_module_messages_en,
+    de: comet_brevo_module_messages_de,
 };
 
 const projectMessages = {
@@ -18,6 +25,7 @@ const projectMessages = {
 export const getMessages = (): ResolvedIntlConfig["messages"] => {
     return {
         ...cometMessages["en"],
+        ...cometBrevoModuleMessages["en"],
         ...projectMessages["en"],
     };
 };
