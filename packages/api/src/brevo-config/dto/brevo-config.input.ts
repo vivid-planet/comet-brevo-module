6edcf98cfa9c76a0294968470f6f51a78dfa4cc8
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-11-28 10:39:11 +0100

    set doiTemplateId to required

--- packages/api/src/brevo-config/dto/brevo-config.input.ts
+++ packages/api/src/brevo-config/dto/brevo-config.input.ts
@@ -1,6 +1,6 @@
-import { IsUndefinable, PartialType } from "@comet/cms-api";
+import { PartialType } from "@comet/cms-api";
 import { Field, InputType, Int } from "@nestjs/graphql";
-import { IsEmail, IsInt, IsNotEmpty, IsString, ValidateIf } from "class-validator";
+import { IsEmail, IsInt, IsNotEmpty, IsString } from "class-validator";
 
 @InputType()
 export class BrevoConfigInput {
@@ -15,11 +15,9 @@ export class BrevoConfigInput {
     @Field()
     senderName: string;
 
-    @IsUndefinable()
     @IsInt()
-    @Field(() => Int, { nullable: true })
-    @ValidateIf((values) => !values.apiKey)
-    doiTemplateId?: number;
+    @Field(() => Int)
+    doiTemplateId: number;
 }
 
 @InputType()
