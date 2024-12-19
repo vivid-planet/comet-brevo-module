ed2a611a8bc2d6447dda214f8b8c11df9ed33cf8
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2024-12-19 14:46:19 +0100

    add styling and helper functions

--- packages/mail-rendering/src/blocks/NewsletterImageBlock.tsx
+++ packages/mail-rendering/src/blocks/NewsletterImageBlock.tsx
@@ -1,7 +1,19 @@
 import { PropsWithData } from "@comet/cms-site";
+import { MjmlColumn, MjmlSection } from "@luma-team/mjml-react";
 
 import { NewsletterImageBlockData } from "../blocks.generated";
+import { CommonImageBlock } from "./CommonImageBlock";
 
-export const NewsletterImageBlock = ({ data }: PropsWithData<NewsletterImageBlockData>) => {
-    return null;
+interface NewsletterImageBlockProps extends PropsWithData<NewsletterImageBlockData> {
+    desktopRenderWidth?: number;
+}
+
+export const NewsletterImageBlock = ({ data, desktopRenderWidth }: NewsletterImageBlockProps) => {
+    return (
+        <MjmlSection>
+            <MjmlColumn>
+                <CommonImageBlock data={data.image} desktopRenderWidth={desktopRenderWidth} />
+            </MjmlColumn>
+        </MjmlSection>
+    );
 };
