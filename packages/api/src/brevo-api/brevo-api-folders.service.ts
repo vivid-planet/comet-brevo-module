7d3bf9b6b4c707625386951aa826ffa8a97a4695
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-02-11 15:55:19 +0100

    add loop to fetch all folders if there are more than 50

--- packages/api/src/brevo-api/brevo-api-folders.service.ts
+++ packages/api/src/brevo-api/brevo-api-folders.service.ts
@@ -4,6 +4,7 @@ import { EmailCampaignScopeInterface } from "src/types";
 
 import { BrevoModuleConfig } from "../config/brevo-module.config";
 import { BREVO_MODULE_CONFIG } from "../config/brevo-module.constants";
+import { handleBrevoError } from "./brevo-api.utils";
 import { BrevoApiFolder } from "./dto/brevo-api-folder";
 
 @Injectable()
@@ -14,21 +15,33 @@ export class BrevoApiFoldersService {
         this.contactsApi = new Brevo.ContactsApi();
     }
 
-    public async getFolders(scope: EmailCampaignScopeInterface): Promise<Array<BrevoApiFolder> | undefined> {
+    async *getAllBrevoFolders(scope: EmailCampaignScopeInterface): AsyncGenerator<BrevoApiFolder, void, undefined> {
         const apiKey = this.config.brevo.resolveConfig(scope).apiKey;
         this.contactsApi.setApiKey(Brevo.ContactsApiApiKeys.apiKey, apiKey);
 
         // Limit set by Brevo
         const limit = 50;
+        let offset = 0;
 
-        const offset = 0;
+        while (true) {
+            try {
+                const { response, body } = await this.contactsApi.getFolders(limit, offset);
 
-        const { response, body } = await this.contactsApi.getFolders(limit, offset);
+                if (response.statusCode !== 200) {
+                    throw new Error("Failed to get folders");
+                }
 
-        if (response.statusCode !== 200) {
-            throw new Error("Failed to get folders");
+                const folders = body.folders ?? [];
+                if (folders.length === 0) {
+                    break;
+                }
+
+                yield* folders;
+
+                offset += limit;
+            } catch (error) {
+                handleBrevoError(error);
+            }
         }
-
-        return body.folders as BrevoApiFolder[];
     }
 }
