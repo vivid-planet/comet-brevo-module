ec1cbe87e9af7f29ab866e5be94ee140f68a82c9
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-04-03 10:29:31 +0200

    store hashed emails in import logs

--- packages/api/src/brevo-email-import-log/brevo-email-import-log.service.ts
+++ packages/api/src/brevo-email-import-log/brevo-email-import-log.service.ts
@@ -1,16 +1,23 @@
 import { EntityManager, EntityRepository } from "@mikro-orm/core";
 import { InjectRepository } from "@mikro-orm/nestjs";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { EmailCampaignScopeInterface } from "src/types";
 
+import { BrevoModuleConfig } from "../config/brevo-module.config";
+import { BREVO_MODULE_CONFIG } from "../config/brevo-module.constants";
+import { hashEmail } from "../util/hash.util";
 import { BrevoEmailImportLogInterface, ContactSource } from "./entity/brevo-email-import-log.entity.factory";
 
 @Injectable()
 export class BrevoEmailImportLogService {
+    private readonly secretKey: string;
     constructor(
+        @Inject(BREVO_MODULE_CONFIG) private readonly config: BrevoModuleConfig,
         @InjectRepository("BrevoEmailImportLog") private readonly repository: EntityRepository<BrevoEmailImportLogInterface>,
         private readonly entityManager: EntityManager,
-    ) {}
+    ) {
+        this.secretKey = this.config.emailHashKey;
+    }
     public async addContactToLogs(
         email: string,
         responsibleUserId: string,
@@ -19,7 +26,7 @@ export class BrevoEmailImportLogService {
         importId?: string,
     ): Promise<BrevoEmailImportLogInterface> {
         const log = this.repository.create({
-            importedEmail: email,
+            importedEmail: hashEmail(email, this.secretKey),
             responsibleUserId,
             scope,
             createdAt: new Date(),
