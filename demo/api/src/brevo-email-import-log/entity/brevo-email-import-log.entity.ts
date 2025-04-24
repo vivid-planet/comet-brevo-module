17d9a1ea19500bcaa2e542d5f46146e42b4185f1
Raphael Blum <raphael.blum@vivid-planet.com>
2025-04-24 14:05:30 +0200

    export new functions createBrevoEmailImportLogEntity and createBlacklistedContactsEntity from index.ts

--- demo/api/src/brevo-email-import-log/entity/brevo-email-import-log.entity.ts
+++ demo/api/src/brevo-email-import-log/entity/brevo-email-import-log.entity.ts
@@ -1,4 +1,4 @@
-import { createBrevoEmailImportLogEntity } from "@comet/brevo-api/lib/brevo-email-import-log/entity/brevo-email-import-log.entity.factory";
+import { createBrevoEmailImportLogEntity } from "@comet/brevo-api";
 import { EmailCampaignContentScope } from "@src/email-campaign/email-campaign-content-scope";
 
 export const BrevoEmailImportLog = createBrevoEmailImportLogEntity({ Scope: EmailCampaignContentScope });
