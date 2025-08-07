79c8e091a2e1728ab0d7dc5af83afd8d8eaa2ad8
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-07 14:42:26 +0200

    update eslint to v9 manually in mail-rendering package

---
+++ packages/mail-rendering/eslint.config.mjs
@@ -0,0 +1,27 @@
+import eslintConfigNextJs from "@comet/eslint-config/nextjs.js";
+
+/** @type {import('eslint')} */
+const config = [
+    {
+        ignores: ["**/**/*.generated.ts", "dist/**", "lang/**", "lang-compiled/**", "lang-extracted/**", ".next/**", "public/**", "lib/**"],
+        rules: {
+            "@comet/no-other-module-relative-import": "off",
+            "react/react-in-jsx-scope": "off",
+            "no-restricted-imports": [
+                "error",
+                {
+                    paths: [
+                        {
+                            name: "react",
+                            importNames: ["default"],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+
+    ...eslintConfigNextJs,
+];
+
+export default config;
