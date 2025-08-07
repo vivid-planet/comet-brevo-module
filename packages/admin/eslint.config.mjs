e5cfd45b832d5325c3d7cc09016159ed8df9e152
Julia Wegmayr <julia.wegmayr@vivid-planet.com>
2025-08-07 14:37:51 +0200

    check for custom rules and remove commet from eslint config in packages

--- packages/admin/eslint.config.mjs
+++ packages/admin/eslint.config.mjs
@@ -1,21 +1,12 @@
-// TODO: integrate custom rules from project into new eslint.config.mjs file
- // 
- // Content from .eslintrc.json:
- //
- // {
-//     "extends": "@comet/eslint-config/react",
-//     "ignorePatterns": ["**/*.generated.ts", "lib/**"],
-//     "rules": {
-//         "@comet/no-other-module-relative-import": "off"
-//     }
-// }
-// 
- import eslintConfigReact from "@comet/eslint-config/react.js";
+import eslintConfigReact from "@comet/eslint-config/react.js";
 
 /** @type {import('eslint')} */
 const config = [
     {
-        ignores: ["schema.json", "src/fragmentTypes.json", "dist/**", "src/**/*.generated.ts"],
+        ignores: ["schema.json", "src/fragmentTypes.json", "dist/**", "src/**/*.generated.ts", "lib/**"],
+        rules: {
+            "@comet/no-other-module-relative-import": "off",
+        },
     },
     ...eslintConfigReact,
 ];
--- packages/api/eslint.config.mjs
+++ packages/api/eslint.config.mjs
@@ -1,21 +1,12 @@
-// TODO: integrate custom rules from project into new eslint.config.mjs file
- // 
- // Content from .eslintrc.json:
- //
- // {
-//     "extends": "@comet/eslint-config/nestjs",
-//     "ignorePatterns": ["lib/**"],
-//     "rules": {
-//         "@comet/no-other-module-relative-import": "off"
-//     }
-// }
-// 
- import eslintConfigNestJs from "@comet/eslint-config/nestjs.js";
+import eslintConfigNestJs from "@comet/eslint-config/nestjs.js";
 
 /** @type {import('eslint')} */
 const config = [
     {
-        ignores: ["src/db/migrations/**", "dist/**", "src/**/*.generated.ts"],
+        ignores: ["src/db/migrations/**", "dist/**", "src/**/*.generated.ts", "lib/**"],
+        rules: {
+            "@comet/no-other-module-relative-import": "off",
+        },
     },
     ...eslintConfigNestJs,
 ];
